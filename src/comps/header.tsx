2efc3f0187775a9f50bf2d2929728cc89732b05c
Crepkey <crepkey@gmail.com>
2022-03-10 10:52:00 +0100

    Header elements are created

--- src/comps/header.tsx
+++ src/comps/header.tsx
@@ -1,9 +1,27 @@
+import styled from "styled-components";
 import vodafoneLogoPath from "../img/vodafone_logo.svg";
+
+const MainContainer = styled.div`
+	display: flex;
+	padding: 10px 0;
+	border: 1px green solid;
+`;
+
+const Logo = styled.img`
+	width: 25%;
+`;
+
+const SearchField = styled.input`
+	border: 1px #cfd4d9 solid;
+	border-radius: 5px;
+	height: 50px;
+`;
 
 export default function Header() {
 	return (
-		<div>
-			<img src={vodafoneLogoPath} alt="This is the official logo of Vodafone company" />
-		</div>
+		<MainContainer>
+			<Logo src={vodafoneLogoPath} alt="This is the official logo of Vodafone company" />
+			<SearchField type="search" placeholder="Search for contact" />
+		</MainContainer>
 	);
 }
