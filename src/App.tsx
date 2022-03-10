2d6f1a9709e36e216b7af46d22f34605fb66543a
Crepkey <crepkey@gmail.com>
2022-03-10 10:29:47 +0100

    Header comp has been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+/* React */
 import { Route, Redirect, Switch } from "react-router-dom";
+import Header from "./comps/header";
+
+/* Components */
 import Test1 from "./comps/test1";
 import Test2 from "./comps/test2";
 
 function App() {
 	return (
 		<div style={{ fontFamily: "Open Sans", fontStyle: "italic" }}>
-			SOMETHING
+			<Header />
 			<Switch>
 				<Route path="/test2" component={Test2} />
 				<Route path="/" exact component={Test1} />
