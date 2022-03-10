d5d617bd271d6434094fdd19a9ae58619925ae5a
Crepkey <crepkey@gmail.com>
2022-03-10 14:34:00 +0100

    Get users from server is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 /* React */
+import { useEffect } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 
 /* Components */
@@ -16,8 +17,44 @@ const MainContainer = styled.div`
 	height: 100vh;
 	width: 100vw;
 `;
+interface UserID {
+	name: string;
+	value: string;
+}
+
+interface Name {
+	first: string;
+	last: string;
+	title: string;
+}
+
+interface UserLocation {
+	city: string;
+	coordinates: { latitude: string; longitude: string };
+	country: string;
+	postcode: number;
+	state: string;
+	street: { number: number; name: string };
+	timezone: { offset: string; description: string };
+}
+interface User {
+	id: UserID;
+	name: Name;
+	phone: string;
+	location: UserLocation;
+}
 
 function App() {
+	useEffect(() => {
+		getUsers();
+	});
+
+	/* FIXME: Error handling is important */
+	async function getUsers() {
+		const response: Response = await fetch(`https://randomuser.me/api/?inc=id,name,email,phone,location&results=50&seed=hire_me`);
+		const parsedResponse: User[] = await response.json();
+		console.log(parsedResponse);
+	}
 	return (
 		<MainContainer>
 			<Header />
