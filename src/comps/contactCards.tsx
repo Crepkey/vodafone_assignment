91d3d11fb2f4916bca1d9403b42309a7316b32b8
Crepkey <crepkey@gmail.com>
2022-03-11 11:34:46 +0100

    Border, pictures, drop shadow for contact cards

--- src/comps/contactCards.tsx
+++ src/comps/contactCards.tsx
@@ -16,16 +16,38 @@ const MainContainer = styled.div`
 	min-height: 0;
 	min-width: 0;
 	overflow: scroll;
-	border: 1px solid green;
+	/* border: 1px solid green; */
 `;
 
 const ContactCard = styled.div`
+	display: flex;
 	width: 30%;
 	min-width: 300px;
+	height: 80px;
 	border: 1px solid ${colors.contactCardBorder};
+	border-radius: 5px;
+	padding: 7px;
+	-webkit-box-shadow: 0px 5px 20px -5px rgba(0, 0, 0, 0.15);
+	box-shadow: 0px 5px 20px -5px rgba(0, 0, 0, 0.15);
 `;
 
-const ContactPicture = styled.img``;
+const PictureContainer = styled.div`
+	display: flex;
+	flex: 1;
+	/* border: 1px solid blue; */
+`;
+
+const ContactPicture = styled.img`
+	width: 80px;
+	height: 80px;
+`;
+
+const DetailsContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	/* border: 1px solid green; */
+`;
 interface ContactCardProps {
 	displayedContacts: Contact[];
 }
@@ -38,8 +60,10 @@ export default function ContactCards({ displayedContacts }: ContactCardProps) {
                 This is not the nicest solution but this is good for quick fix.
                 */
 				<ContactCard key={contact.id.value + index}>
-					<ContactPicture src={contact.picture.large} alt="This is the profile picture of the given contact" />
-					{contact.email}
+					<PictureContainer>
+						<ContactPicture src={contact.picture.large} alt="This is the profile picture of the given contact" />
+					</PictureContainer>
+					<DetailsContainer>{contact.email}</DetailsContainer>
 				</ContactCard>
 			))}
 		</MainContainer>
