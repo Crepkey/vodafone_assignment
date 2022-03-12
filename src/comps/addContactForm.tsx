f9b454e44e8c90db1afef6c8dea05fc47b63e1f5
Crepkey <crepkey@gmail.com>
2022-03-12 14:32:21 +0100

    Form fields are under control

--- src/comps/addContactForm.tsx
+++ src/comps/addContactForm.tsx
@@ -1,8 +1,9 @@
 /* React */
-import { useState } from "react";
+import React, { useState } from "react";
 
 /* Utils */
 import { emptyContact } from "../utils/utils";
+import set from "lodash/set";
 
 /* Components */
 import PageTitle from "./pageTitle";
@@ -64,10 +65,19 @@ const AddContactButton = styled.button`
 `;
 
 export default function AddContactForm() {
-	const [newContact, setNewContact] = useState<NewContact>(emptyContact);
+	const [contact, setContact] = useState<NewContact>(emptyContact);
+
 	const handleSubmit = (event: React.SyntheticEvent) => {
 		event.preventDefault();
 		console.log("Submitted");
+	};
+
+	/* TODO: Typescript type for event */
+	const handleChange = (event: any) => {
+		console.log(event);
+		const newContact: NewContact = { ...contact };
+		set(newContact, event.currentTarget.name, event.currentTarget.value);
+		setContact(newContact);
 	};
 
 	return (
@@ -75,15 +85,15 @@ export default function AddContactForm() {
 			<PageTitle text="Add New Contact" />
 			<Form onSubmit={handleSubmit}>
 				<Label htmlFor="first_name">First Name</Label>
-				<Input type="text" id="first_name" placeholder="Enter first name" />
+				<Input type="text" id="first_name" placeholder="Enter first name" name="name.first" onChange={handleChange} />
 				<Label htmlFor="last_name">Last Name</Label>
-				<Input type="text" id="last_name" placeholder="Enter last name" />
+				<Input type="text" id="last_name" placeholder="Enter last name" name="name.last" onChange={handleChange} />
 				<Label htmlFor="email">Email</Label>
-				<Input type="text" id="email" placeholder="Enter Email" />
+				<Input type="text" id="email" placeholder="Enter Email" name="email" onChange={handleChange} />
 				<Label htmlFor="phone">Phone</Label>
-				<Input type="text" id="phone" placeholder="Enter Phone" />
+				<Input type="text" id="phone" placeholder="Enter Phone" name="phone" onChange={handleChange} />
 				<Label htmlFor="address">Address</Label>
-				<Input type="text" id="address" placeholder="Enter Address" />
+				<Input type="text" id="address" placeholder="Enter Address" name="location.street" onChange={handleChange} />
 				<AddContactButton>Add contact</AddContactButton>
 			</Form>
 		</MainContainer>
