f11676a4c77239f151ca70d67035f1f34a21e169
Crepkey <crepkey@gmail.com>
2022-03-16 17:34:33 +0100

    Info Page uses the new general button

--- src/comps/common/InfoPage.tsx
+++ src/comps/common/InfoPage.tsx
@@ -10,6 +10,7 @@ import PageTitle from "../pageTitle";
 /* Styles */
 import styled from "styled-components";
 import { colors } from "../../utils/colors";
+import Button from "./Button";
 
 const MainContainer = styled.div`
 	display: flex;
@@ -32,26 +33,6 @@ const Details = styled.div`
 	padding: 20px;
 	max-width: 800px;
 `;
-
-const Button = styled.button`
-	padding: 6px 10px;
-	color: ${colors.red};
-	background-color: rgba(0, 0, 0, 0);
-	border: 1px solid ${colors.red};
-	border-radius: 5px;
-	font-size: 0.9rem;
-	margin: 15px;
-	transition: all 0.3s ease;
-	:hover {
-		cursor: pointer;
-		color: white;
-		background-color: ${colors.red};
-	}
-	:active {
-		color: ${colors.red};
-		background-color: rgba(0, 0, 0, 0);
-	}
-`;
 interface ErrorPageProps {
 	icon: false | "error";
 	title: string;
@@ -61,7 +42,7 @@ interface ErrorPageProps {
 	URLForRedirection?: string;
 }
 
-export default function InfoPage({ icon, title, details, button, buttonText, URLForRedirection = "" }: ErrorPageProps) {
+export default function InfoPage({ icon, title, details, button, buttonText = "", URLForRedirection = "" }: ErrorPageProps) {
 	function generateIcon() {
 		switch (icon) {
 			case false:
@@ -84,7 +65,7 @@ export default function InfoPage({ icon, title, details, button, buttonText, URL
 			<Details>{details}</Details>
 			{button && (
 				<Link to={URLForRedirection}>
-					<Button>{buttonText}</Button>
+					<Button style={{ margin: "15px" }} colorStyle={"common"} text={buttonText} />
 				</Link>
 			)}
 		</MainContainer>
